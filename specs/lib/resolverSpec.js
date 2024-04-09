53535f1de6483b09bf9dc3d0e7b87a582e4b3ae8
Wouter Kayser <wouterkayser@gmail.com>
2024-04-09 19:22:34 +0200

    resolveFile always returb buffer from source

--- specs/lib/resolverSpec.js
+++ specs/lib/resolverSpec.js
@@ -1,18 +1,17 @@
 import path from "path";
 import { promises as fsPromises } from "fs";
 
-export async function resolveFile(texturePath, mtlPath, handle = false) {
+export async function resolveFile(texturePath, mtlPath) {
   const mtlDirectory = path.dirname(mtlPath || "");
   texturePath = path.normalize(path.resolve(mtlDirectory, texturePath));
-  const fs_function = handle ? fsPromises.open : fsPromises.readFile;
 
   let source;
   try {
-    source = await fs_function(texturePath);
+    source = await fsPromises.readFile(texturePath);
   } catch {
     const shallowPath = path.join(mtlDirectory, path.basename(texturePath));
     try {
-      source = await fs_function(shallowPath);
+      source = await fsPromises.readFile(shallowPath);
     } catch {
       console.info("file doesnt exist");
     }
